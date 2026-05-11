4afcc666baca18edd461c92f00f8d8d8b9cd6319
Evan Bacon <baconbrix@gmail.com>
2026-05-11 13:19:38 -0700

    remove issue from merge

--- packages/serve-sim/src/client/location-emulation-icons/index.ts
+++
@@ -1,3 +0,0 @@
-export { Chevron, ArrowGlyph } from "./chrome";
-export { PlayGlyph, PauseGlyph, StopGlyph, FastForwardGlyph } from "./playback";
-export { WalkGlyph, RunGlyph, CycleGlyph, DriveGlyph } from "./modes";
