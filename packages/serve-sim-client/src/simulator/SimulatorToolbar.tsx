aefe394531e348abd637cdf2ececebaa991f37b4
Evan Bacon <baconbrix@gmail.com>
2026-06-11 15:23:22 -0700

    Update SimulatorToolbar.tsx

--- packages/serve-sim-client/src/simulator/SimulatorToolbar.tsx
+++ packages/serve-sim-client/src/simulator/SimulatorToolbar.tsx
@@ -334,7 +334,7 @@ const ScreenshotIcon = (
 );
 
 const RotateIcon = (
-  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg">
+  <svg width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg">
     <path d="M12 5H6a2 2 0 0 0-2 2v3" />
     <path d="m9 8 3-3-3-3" />
     <path d="M4 14v4a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2" />
